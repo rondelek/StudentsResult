89d7f90f4f4cd00445ae5576a12d2605ba7e6e2c
Ewelina Nowak <ewelinanowak@192.168.1.3>
2024-03-15 19:12:20 +0100

    model trainer

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,8 @@ import os
 import sys
 import dill
 
+from sklearn.metrics import r2_score
+
 import numpy as np
 import pandas as pd
 
@@ -39,3 +41,34 @@ Ta funkcja służy do zapisywania obiektów Pythona do pliku za pomocą modułu 
    - Jeśli wystąpi błąd podczas wykonywania któregokolwiek z powyższych kroków, zostanie wywołany wyjątek. Wyjątek jest przechwytywany przez blok `except`, a następnie jest rzucany niestandardowy wyjątek `CustomException`, który został zdefiniowany gdzie indziej w kodzie.
    - Informacje o błędzie są przekazywane do `CustomException`, aby ułatwić diagnozowanie problemów zapisu obiektu.
 '''
+
+def evaluate_models(X_train, y_train,X_test,y_test,models):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            # para=param[list(models.keys())[i]]
+
+            # gs = GridSearchCV(model,para,cv=3)
+            # gs.fit(X_train,y_train)
+
+            # model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
+
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
