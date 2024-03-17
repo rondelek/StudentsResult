8c2374786bee69bf6e4ab6ce56ddb3066bcdd42a
Ewelina Nowak <ewelinanowak@192.168.1.3>
2024-03-17 23:57:59 +0100

    added predict pipeline

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,8 @@ import sys
 import dill
 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+
 
 import numpy as np
 import pandas as pd
@@ -42,21 +44,19 @@ Ta funkcja służy do zapisywania obiektów Pythona do pliku za pomocą modułu 
    - Informacje o błędzie są przekazywane do `CustomException`, aby ułatwić diagnozowanie problemów zapisu obiektu.
 '''
 
-def evaluate_models(X_train, y_train,X_test,y_test,models):
+def evaluate_models(X_train, y_train,X_test,y_test,models, param):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            # para=param[list(models.keys())[i]]
+            para=param[list(models.keys())[i]]
 
-            # gs = GridSearchCV(model,para,cv=3)
-            # gs.fit(X_train,y_train)
+            gs = GridSearchCV(model,para,cv=5)
+            gs.fit(X_train,y_train)
 
-            # model.set_params(**gs.best_params_)
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
-
-            #model.fit(X_train, y_train)  # Train model
 
             y_train_pred = model.predict(X_train)
 
@@ -70,5 +70,14 @@ def evaluate_models(X_train, y_train,X_test,y_test,models):
 
         return report
 
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+def load_object(file_path):
+    try:
+        with open(file_path, 'rb') as file_obj:
+            return dill.load(file_obj)
+        
     except Exception as e:
         raise CustomException(e, sys)
